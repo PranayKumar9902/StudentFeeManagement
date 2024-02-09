4e4380d892ab568fd4be26bdcf2934e85e37d3b0
PranayKumar9902 <123971170+PranayKumar9902@users.noreply.github.com>
2024-02-09 21:47:16 +0530

    Update AddAuthor.java

--- src/com/add/AddAuthor.java
+++ src/com/add/AddAuthor.java
@@ -30,7 +30,7 @@ public class AddAuthor extends HttpServlet{
             String pass = request.getParameter("pass");
             
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.24:1521:orclnew1", "C##TRAINEES_DATABASE", "system123#");
+            Connection con = DriverManager.getConnection("DBURL", "DBNAME", "PASSWORD");
             PreparedStatement st = con.prepareStatement("INSERT INTO BUNTY_AUTHORS VALUES(?,?)");
             st.setString(1,uname);
             st.setString(2, pass);
