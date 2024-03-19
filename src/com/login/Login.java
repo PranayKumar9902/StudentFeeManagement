751dfb413463b8189e7ba9906256a2037ba7dc13
PranayKumar9902 <123971170+PranayKumar9902@users.noreply.github.com>
2024-03-20 00:14:21 +0530

    Update Login.java

--- src/com/login/Login.java
+++ src/com/login/Login.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.login;
 
 import java.io.IOException;
@@ -20,10 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author TR080124021
- */
+
 @WebServlet("/login")
 public class Login extends HttpServlet{
     
