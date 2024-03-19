998753c6633f46064c406e1e1e238f18df51daf1
PranayKumar9902 <123971170+PranayKumar9902@users.noreply.github.com>
2024-03-20 00:13:55 +0530

    Update Delete.java

--- src/com/delete/Delete.java
+++ src/com/delete/Delete.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.delete;
 
 import java.io.IOException;
@@ -18,10 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author TR080124021
- */
+
 @WebServlet("/Delete")
 public class Delete extends HttpServlet{
     
