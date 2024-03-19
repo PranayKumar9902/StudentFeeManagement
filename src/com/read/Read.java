bfbf7d99739882125f973855201bd77767072e34
PranayKumar9902 <123971170+PranayKumar9902@users.noreply.github.com>
2024-03-20 00:15:40 +0530

    Update Read.java

--- src/com/read/Read.java
+++ src/com/read/Read.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.read;
 
 import com.google.gson.Gson;
@@ -21,10 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author TR080124021
- */
+
 @WebServlet("/Read")
 public class Read extends HttpServlet{
     
